e02f01ca65b5908fcfcb917e47dc2a469cb8fc0d
ninjaPixel <matthew@ninjaPixel.eu>
2017-04-25 00:27:27 +0100

    Working app

--- TripRevisor/app/components/CityDetails.js
+++ TripRevisor/app/components/CityDetails.js
@@ -1,10 +1,77 @@
-import React from 'react';
-import { ScrollView, Text } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
+import Meteor from 'react-native-meteor';
+import { View, ScrollView, Text, Image } from 'react-native';
 
-const CityDetails = props => (
-  <ScrollView>
-    <Text>Hello!</Text>
-  </ScrollView>
-);
+const styles = StyleSheet.create({
+  text: {
+    padding: 15,
+  },
+  image: {
+    width: 400,
+    height: 400,
+  },
+});
+
+class CityDetails extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+      error: '',
+      city: {},
+    };
+  }
+
+  componentDidMount() {
+    const params = { name: this.props.navigation.state.params.name };
+    Meteor.call('cities.findOne', params, (err, cities) => {
+      if (err) {
+        this.setState({ error: err.reason });
+      } else {
+        this.setState({ error: '', city: cities });
+      }
+      this.setState({ loading: false });
+    });
+  }
+
+  renderImages() {
+    return this.state.city.photos.map((url, i) => (
+      <Image
+        key={i} source={{ uri: url }}
+        style={styles.image}
+      />
+        ));
+  }
+
+  renderCityDetails() {
+    return (
+      <View>
+        <ScrollView horizontal>
+          {this.renderImages()}
+        </ScrollView>
+        <Text style={styles.text}>{this.state.city.text}</Text>
+      </View>
+    );
+  }
+
+  render() {
+    const isLoading = this.state.loading;
+    const error = this.state.error;
+    return (
+      <ScrollView>
+        { isLoading ? (
+          <Text>Loading data for {this.props.navigation.state.params.name}...</Text>
+        ) : this.renderCityDetails()
+        }
+
+        {error ? (
+          <Text>{error}</Text>
+        ) : (null)}
+      </ScrollView>
+    );
+  }
+}
 
 export default CityDetails;
--- TripRevisor/app/components/Navigator.js
+++ TripRevisor/app/components/Navigator.js
@@ -3,8 +3,8 @@ import Meteor from 'react-native-meteor';
 import HomeScreen from './HomeScreen';
 import CityDetails from './CityDetails';
 
-// Meteor.connect('ws://192.168.1.66:3000/websocket');
-// Meteor.connect('ws://localhost:3000/websocket');
+// replace this with your IP address, or the address of the server you are targeting.
+Meteor.connect('ws://localhost:3000/websocket');
 
 const Navigator = StackNavigator({
   Home: {
